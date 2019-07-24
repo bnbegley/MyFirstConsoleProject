161f255536a0b1bb5aab410cbc3c56d7f45d64cf
brail33na11@gmail.com <brail33na11@gmail.com>
2019-07-24 11:15:04 -0400

    Added the IsEvenlyDivisible method calling Mod method

--- MyFirstConsoleProject/Program.cs
+++ MyFirstConsoleProject/Program.cs
@@ -14,8 +14,10 @@ namespace MyFirstConsoleProject
             Console.WriteLine("6 * 3 = " + answer);
             answer = Arithmetic.Div(8, 2);
             Console.WriteLine("8 / 2 = " + answer);
-
-
+            answer = Arithmetic.Mod(2546, 111);
+            //Console.WriteLine("10 % 4 = " + answer);
+            bool TorF = Arithmetic.IsEvenlyDivisible(655, 5);
+            Console.WriteLine($"655 is evenly divisible by 5: {TorF}");
         }
     }
     class Arithmetic {
@@ -25,7 +27,20 @@ namespace MyFirstConsoleProject
         public static int Sub(int a, int b){
             return a - b;
         }
-        public static int Mult(int a, int b) { return a * b; }
-        public static int Div(int a, int b) { return a / b; }
+        public static int Mult(int a, int b) {
+            return a * b;
+        }
+       public static int Div(int a, int b) {
+            return a / b;
+        }
+        public static int Mod(int a, int b) {
+            int ans = a % b;
+            Console.WriteLine($"{a} mod {b} = {ans}");
+            return ans;
+        }
+        public static bool IsEvenlyDivisible(int a, int b)
+        {
+            return Mod(a, b) == 0;
+        }
     }
 }
